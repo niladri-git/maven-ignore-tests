71d0999cd0635aca72e478b6a6d0b69ac311bca5
Niladri Datta <dattaniladri@gmail.com>
2015-08-27 02:29:50 +0530

    Simple maven project and skip test cases info

---
+++ src/main/java/com/mkyong/core/App.java
@@ -0,0 +1,21 @@
+package com.mkyong.core;
+
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println(getHelloWorld());
+
+	}
+
+	public static String getHelloWorld() {
+
+		return "Hello World";
+
+	}
+
+	public static String getHelloWorld2() {
+
+		return "Hello World 2";
+
+	}
+}
